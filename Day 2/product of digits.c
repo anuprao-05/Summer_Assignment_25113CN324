9a41e39905b0457eda8edd44a57cb10720f5a359
anuprao-05 <aanup2784@gmail.com>
2026-06-02 17:06:34 +0530

    Day 2 uploaded

---
+++ Day 2/product of digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num, digit, product = 1;
+
+    printf("Enter a number : ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        num = -num;
+
+    while (num != 0) {
+        digit   = num % 10;
+        product = product * digit;
+        num     = num / 10;
+    }
+
+    printf("Product of Digits = %d\n", product);
+
+    return 0;
+}
