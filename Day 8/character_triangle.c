846209accde0a23badfbeac23310d09d3925d84f
anuprao-05 <aanup2784@gmail.com>
2026-06-08 19:45:43 +0530

    Day 8 uploaded

---
+++ Day 8/character_triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+
+    printf("\n");
+
+    for (i = 1; i <= n; i++) {
+        for (j = 0; j < i; j++)
+            printf("%c", 'A' + j);
+        printf("\n");
+    }
+
+    return 0;
+}
